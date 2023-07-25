ab94eabc4e2ead63c9f5fecc665f5eda7d7a2843
Remco Stoeten <remcostoeten@hotmail.com>
2023-07-25 22:31:29 +0200

    xx

--- app/dashboard-two/page.jsx
+++ app/dashboard-two/page.jsx
@@ -31,8 +31,11 @@ export default function Dashboard() {
   const [notes, setNotes] = useState([])
   const [loading, setLoading] = useState(true)
   const [editModeMap, setEditModeMap] = useState({})
-  const [filteredNotes, setFilteredNotes] = useState([])
-
+  const [hiddenCategories, setHiddenCategories] = useState([])
+  const [showFilters, setShowFilters] = useState(true) // Toggle to show/hide filters
+  const handleFilter = () => {
+    setShowFilters((prevShowFilters) => !prevShowFilters)
+  }
   const username = auth.currentUser
   const user = auth.currentUser
 
@@ -45,24 +48,37 @@ export default function Dashboard() {
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
-        fetchNotes(user, category) // Fetch notes with the selected category
+        fetchNotes(user)
       } else {
         router.push("/login")
       }
-      setLoading(false) // Set loading to false after auth state has been checked
+      setLoading(false)
     })
 
     return () => unsubscribe()
-  }, [category])
+  }, [])
 
   const fetchNotes = async (user) => {
     if (user && user.uid) {
       try {
-        const notesQuery = query(
+        let notesQuery = query(
           collection(db, "notes"),
           where("userId", "==", user.uid),
           orderBy("createdAt", "desc")
         )
+
+        const visibleCategories = categories
+          .filter((category) => !hiddenCategories.includes(category.name))
+          .map((category) => category.name)
+
+        if (visibleCategories.length > 0) {
+          notesQuery = query(
+            collection(db, "notes"),
+            where("userId", "==", user.uid),
+            where("category", "in", visibleCategories),
+            orderBy("createdAt", "desc")
+          )
+        }
 
         const snapshot = await getDocs(notesQuery)
         const fetchedNotes = snapshot.docs.map((doc) => {
@@ -73,29 +89,11 @@ export default function Dashboard() {
             createdAt: data.createdAt ? new Date(data.createdAt) : null,
           }
         })
-
-        setNotes(fetchedNotes) // Store all notes in state
-        setFilteredNotes(fetchedNotes) // Initialize filtered notes with all notes
+        setNotes(fetchedNotes)
       } catch (error) {
         console.log("Error fetching notes:", error)
       }
     }
-  }
-
-  const handleCategoryChange = (e) => {
-    const selectedCategory = e.target.value
-
-    // Filter notes based on the selected category
-    const filtered = notes.filter((note) => {
-      if (selectedCategory === "") {
-        return true // Show all notes when the "All" option is selected
-      } else {
-        return note.category === selectedCategory
-      }
-    })
-
-    setCategory(selectedCategory)
-    setFilteredNotes(filtered)
   }
 
   const handleSubmit = async (e) => {
@@ -130,61 +128,118 @@ export default function Dashboard() {
     }
   }
 
-  const handleFilter = () => {
-    fetchNotes(user, category)
+  const handleRemove = async (id) => {
+    try {
+      await deleteDoc(doc(db, "notes", id))
+      setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id))
+
+      toast({
+        title: "Note removed successfully.",
+      })
+    } catch (error) {
+      toast({
+        title: "Couldn't remove note.",
+        variant: "destructive",
+      })
+      console.error(error)
+    }
+  }
+
+  const toggleEditMode = (id) => {
+    setEditModeMap((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }))
+  }
+
+  const handleEdit = async (note) => {
+    try {
+      await updateDoc(doc(db, "notes", note.id), {
+        title: note.title,
+        content: note.content,
+      })
+
+      toggleEditMode(note.id)
+
+      toast({
+        title: "Note updated successfully.",
+      })
+    } catch (error) {
+      toast({
+        title: "Couldn't update note.",
+        variant: "destructive",
+      })
+      console.error(error)
+    }
+  }
+
+  const handleCategoryToggle = (e) => {
+    const categoryName = e.target.value
+    setHiddenCategories((prevHiddenCategories) => {
+      if (prevHiddenCategories.includes(categoryName)) {
+        return prevHiddenCategories.filter(
+          (category) => category !== categoryName
+        )
+      } else {
+        return [...prevHiddenCategories, categoryName]
+      }
+    })
+    fetchNotes(user)
   }
 
   return (
     <>
       <div className="mx-auto w-3/6 mt-4 max-w-3xl">
         <div className="grid items-start gap-8">
-          <div className="flex flex-col gap-2 px-2">
-            <div className="grid gap-1">
-              <h1 className="font-heading text-3xl md:text-4xl">Posts</h1>
-              <p className="text-lg text-muted-foreground">
-                Create and manage posts.
-              </p>
-            </div>
-            Welcome {username?.email}
-            <form className="flex gap-2 flex-col" onSubmit={handleSubmit}>
-              <Input
-                type="text"
-                placeholder="Title"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-              <select value={category} onChange={handleCategoryChange}>
-                <option value="">All</option>
+          {showFilters && (
+            <div className="flex flex-col gap-2 px-2">
+              <div className="grid gap-1">
+                <h1 className="font-heading text-3xl md:text-4xl">Posts</h1>
+                <p className="text-lg text-muted-foreground">
+                  Create and manage posts.
+                </p>
+              </div>
+              <form className="flex gap-2 flex-col" onSubmit={handleSubmit}>
+                <Input
+                  type="text"
+                  placeholder="Title"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
                 {categories.map((category) => (
-                  <option key={category.id} value={category.name}>
+                  <label key={category.id} className="flex items-center gap-2">
+                    <input
+                      type="checkbox"
+                      value={category.name}
+                      onChange={handleCategoryToggle}
+                      checked={!hiddenCategories.includes(category.name)}
+                    />
                     {category.name}
-                  </option>
+                  </label>
                 ))}
-              </select>
-              <Textarea
-                placeholder="Note content"
-                value={content}
-                onChange={(e) => setContent(e.target.value)}
-              />
-
-              <Button
-                className={cn(
-                  buttonVariants({
-                    variant: "primary",
-                    color: "primary",
-                    size: "md",
-                  })
-                )}
-                onClick={handleSubmit}
-                type="submit"
-              >
-                New post
-              </Button>
-            </form>
-          </div>
+                <Textarea
+                  placeholder="Note content"
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
+                />
+                <Button
+                  className={cn(
+                    buttonVariants({
+                      variant: "primary",
+                      color: "primary",
+                      size: "md",
+                    })
+                  )}
+                  onClick={handleSubmit}
+                  type="submit"
+                >
+                  New post
+                </Button>
+              </form>
+            </div>
+          )}
           <div>
-            {/* Display the filtered notes instead of all notes */}
-            {filteredNotes.map((note) => (
+            {notes.map((note) => (
               <div
                 key={note.id}
                 className="divide-y divide-border rounded-md border"
@@ -287,6 +342,19 @@ export default function Dashboard() {
                 </Link>
                 <p className="text-sm text-muted-foreground">dddd </p>
               </div>
+              <Button
+                className={cn(
+                  buttonVariants({
+                    variant: "primary",
+                    color: "primary",
+                    size: "md",
+                  })
+                )}
+                onClick={handleFilter} // Add the handleFilter function to the onClick event
+                type="button" // Change the type to "button" to prevent form submission
+              >
+                Filter
+              </Button>
               <Button
                 type="submit"
                 className={cn(
