7ac24feca4f5963071b035c89c0caac005c19570
Remco Stoeten <remcostoeten@hotmail.com>
2023-07-29 16:03:38 +0200

    rm

--- app/productivity-tools/loading.tsx
+++
@@ -1,6 +0,0 @@
-import LoadingAnimation from "@/components/loadingAnimation"
-
-export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <LoadingAnimation />
-}
