36d9278e498de64ad69bbf795c16fedb83a23124
Remco Stoeten <remcostoeten@hotmail.com>
2023-08-06 18:34:43 +0200

    Add tsx

--- app/html-to-jsx/page.tsx
+++ app/html-to-jsx/page.tsx
@@ -1,38 +1,33 @@
-"use client";
+'use client';
 import { useState, useCallback } from "react";
+import { Editor } from '@monaco-editor/react';
 
-
-function convertHtmlToJSX(html: string) {
-    // Convert class to className and for to htmlFor
+function convertHtmlToJSX(html: string): string {
     let jsx = html.replace(/\bclass=/g, "className=");
     jsx = jsx.replace(/\bfor=/g, "htmlFor=");
 
-    // Convert onclick to onClick, onmouseover to onMouseOver, etc.
     jsx = jsx.replace(/\bon([a-z]+)/g, function (match: any, group: string) {
         return "on" + group.charAt(0).toUpperCase() + group.slice(1);
     });
 
-    // Convert unquoted attributes to quoted attributes
     jsx = jsx.replace(/=(\w+)/g, function (match: any, group: string) {
         return `="${group}"`;
     });
 
-    // Convert inline styles to objects
     jsx = jsx.replace(/style="([^"]*)"/g, function (match: any, group: string) {
         let style = group.split(";").reduce(function (style, rule) {
             let parts = rule.split(":");
             if (parts[1]) {
                 let key = parts[0].trim();
                 let value = parts[1].trim();
-                // Detect if value is numeric
                 if (!isNaN(value as any)) {
                     value = parseInt(value).toString();
                 } else if (value !== "true" && value !== "false") {
-                    value = `'${value}'`; // Keep as string if not boolean
+                    value = `'${value}'`;
                 }
                 key = key.replace(/-./g, function (x) {
                     return x[1].toUpperCase();
-                }); // Convert kebab-case to camelCase
+                });
                 style += key + ": " + value + ", ";
             }
             return style;
@@ -40,38 +35,23 @@ function convertHtmlToJSX(html: string) {
         return `style={{${style}}}`;
     });
 
-    // Convert boolean attributes
-    const booleanAttributes = [
-        "checked",
-        "selected",
-        "disabled",
-        "readOnly",
-        "multiple",
-        "hidden",
-    ];
+    const booleanAttributes = ["checked", "selected", "disabled", "readOnly", "multiple", "hidden"];
     booleanAttributes.forEach((attr) => {
-        const re = new RegExp(
-            `<([a-zA-Z][a-zA-Z0-9]*)\\b[^>]*?\\b${attr}(?![^>]*?>)`,
-            "g"
-        );
+        const re = new RegExp(`<([a-zA-Z][a-zA-Z0-9]*)\\b[^>]*?\\b${attr}(?![^>]*?>)`, "g");
         jsx = jsx.replace(re, `<$1 ${attr}={true}`);
     });
 
-    // Ensure self-closing tags are properly closed
     const selfClosingTags = ["br", "hr", "img", "input", "link", "meta"];
     selfClosingTags.forEach((tag) => {
         const re = new RegExp(`<${tag}\\b([^>]*)(?<!/)>`, "g");
         jsx = jsx.replace(re, `<${tag}$1 />`);
     });
 
-    // Replace HTML comments with JSX comments
     jsx = jsx.replace(/<!--([\s\S]*?)-->/g, function (match: any, group: string) {
-        // Convert each line into a separate JSX comment
         const lines = group.split("\n");
         return lines.map((line) => `{/*${line.trim()}*/}`).join("\n");
     });
 
-    // Convert SVG kebab-case attributes to camelCase
     jsx = jsx.replace(
         /<(svg|path|circle|rect|line|polyline|polygon|text|g|defs|use|mask)[^>]*>/g,
         function (match: any) {
@@ -81,16 +61,15 @@ function convertHtmlToJSX(html: string) {
         }
     );
 
-    // Replace HTML entities within the attribute values
     jsx = jsx.replace(/&amp;/g, "");
 
     return jsx;
 }
 
-export default function Home() {
+const Home: React.FC = () => {
     const [code, setCode] = useState<string | undefined>("// Enter HTML here");
-    const [jsx, setJSX] = useState("");
-    const [showNotification, setShowNotification] = useState(false);
+    const [jsx, setJSX] = useState<string>("");
+    const [showNotification, setShowNotification] = useState<boolean>(false);
 
     const handleCopy = useCallback(() => {
         navigator.clipboard
@@ -98,7 +77,7 @@ export default function Home() {
             .then(() => {
                 console.log("Copying to clipboard was successful!");
                 setShowNotification(true);
-                setTimeout(() => setShowNotification(false), 5000); // hide notification after 5 seconds
+                setTimeout(() => setShowNotification(false), 5000);
             })
             .catch((err) => {
                 console.error("Could not copy text: ", err);
@@ -206,10 +185,8 @@ export default function Home() {
                                 onChange={handleEditorChange}
                             />
                             <div className="relative">
-                                {" "}
-                                {/* added a relative positioned div */}
                                 <button
-                                    onClick={handleCopy} // Add onClick handler here
+                                    onClick={handleCopy}
                                     className="absolute p-2 bg-zinc-900 border rounded-md border-zinc-700 top-0 right-0 z-50"
                                 >
                                     <svg
@@ -241,4 +218,6 @@ export default function Home() {
             </div>
         </div>
     );
-}
+};
+
+export default Home;
