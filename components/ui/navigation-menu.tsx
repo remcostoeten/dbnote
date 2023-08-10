97afc75ff1515bea922fea1f48ce7bd0b57f76f9
Remco Stoeten <remcostoeten@hotmail.com>
2023-08-10 20:28:57 +0200

    Update menu

--- components/ui/navigation-menu.tsx
+++ components/ui/navigation-menu.tsx
@@ -83,7 +83,7 @@ const NavigationMenuViewport = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Viewport>,
   React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Viewport>
 >(({ className, ...props }, ref) => (
-  <div className={cn("absolute topt-[70px]  flex justify-center z-max")}>
+  <div className={cn("absolute translate-y-[320px]  flex justify-center z-max")}>
     <NavigationMenuPrimitive.Viewport
       className={cn(
         "origin-top-center relative mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[var(--radix-navigation-menu-viewport-width)]",
