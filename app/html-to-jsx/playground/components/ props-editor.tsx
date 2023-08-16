77e8c9ab61dc2222c162518a4cd589d7e05d1c99
Remco Stoeten <remcostoeten@hotmail.com>
2023-08-16 22:33:04 +0200

    WiP

--- app/html-to-jsx/playground/components/ props-editor.tsx
+++ app/html-to-jsx/playground/components/ props-editor.tsx
@@ -1,20 +1,61 @@
-import { Input } from "@/components/ui/input"
+import { PlusIcon } from "@radix-ui/react-icons"
+import { motion } from "framer-motion"
+import React, { Fragment } from "react"
 
-const PropsEditor = ({ propsArray, handlePropChange, addNewProp }) => {
+const PropsEditor = ({
+  removeProp,
+  propsArray,
+  handlePropChange,
+  addNewProp,
+}) => {
   return (
-    <div>
-      {propsArray.map((propValue, index) => (
-        <div key={index}>
-          <Input
-            type="text"
-            value={propValue}
-            onChange={(e) => handlePropChange(e, index)}
-            placeholder="Enter prop (e.g. name: string)"
-          />
+    <Fragment>
+      <motion.div
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, y: 10 }}
+        transition={{ type: "spring", stiffness: 150, damping: 15, delay: 1 }}
+        className="ml-4 flex "
+      >
+        <div className="flex w-full items-center">
+          {propsArray.map((propValue, index) => (
+            <motion.div
+              initial={{ width: 0, height: 0 }}
+              animate={{ width: 170, height: 40 }}
+              exit={{ opacity: 0, y: 10 }}
+              transition={{ type: "spring", stiffness: 150, damping: 15 }}
+            >
+              <motion.input
+                key={index}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, y: 10 }}
+                transition={{ type: "spring", stiffness: 150, damping: 15 }}
+                onHover={{ scale: 1.1 }}
+                type="text"
+                value={propValue}
+                className="flex h-10 w-[150px] rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 "
+                onChange={(e) => handlePropChange(e, index)}
+                placeholder="Enter prop (e.g. name: string)"
+              />
+            </motion.div>
+          ))}
         </div>
-      ))}
-      <button onClick={addNewProp}>+</button>
-    </div>
+        <button
+          className="inline-flex h-[40px] w-[40px] items-center justify-center rounded-md border border-dashed border-input bg-transparent px-3 text-xs font-medium shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+          onClick={addNewProp}
+        >
+          <PlusIcon />
+        </button>
+        {propsArray.length > 1 && (
+          <button
+            className="-mt-2 ml-2 inline-flex h-[40px] w-[40px] -translate-y-1 items-center justify-center rounded-md border border-dashed border-input bg-transparent px-3 text-xs font-medium shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+            onClick={removeProp}
+          >
+            -
+          </button>
+        )}{" "}
+      </motion.div>
+    </Fragment>
   )
 }
 
--- app/html-to-jsx/playground/page.tsx
+++ app/html-to-jsx/playground/page.tsx
@@ -24,6 +24,8 @@ import { PresetSelector } from "./components/preset-selector"
 import { PresetShare } from "./components/preset-share"
 import { models, types } from "./data/models"
 import { presets } from "./data/presets"
+import { Badge } from "@/components/ui/badge"
+import { motion } from "framer-motion"
 
 export default function PlaygroundPage() {
   const [code, setCode] = useState<string | undefined>("// Enter HTML here")
@@ -219,6 +221,12 @@ export default function PlaygroundPage() {
     setPropsArray([...propsArray, ""])
   }
 
+  function removeProp(index: number) {
+    const updatedProps = [...propsArray]
+    updatedProps.splice(index, 1)
+    setPropsArray(updatedProps)
+  }
+
   return (
     <>
       <AppContext.Provider
@@ -235,7 +243,24 @@ export default function PlaygroundPage() {
           setWrapInFunctionComponent,
         }}
       >
-        <div className="overflow-hidden rounded-[0.5rem] border bg-background shadow">
+        <motion.div
+          initial={{ y: -10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ opacity: 0, y: 10 }}
+          transition={{
+            type: "spring",
+            stiffness: 150,
+            damping: 15,
+            delay: 0.5,
+          }}
+          className="overflow-hidden rounded-[0.5rem] border bg-background shadow"
+        >
+          <Badge
+            variant="default"
+            className="transform-translate-x-3 absolute  right-0 -translate-x-12 translate-y-3"
+          >
+            Beta
+          </Badge>
           <div className="md:hidden">
             <Image
               src="/examples/playground-light.png"
@@ -252,33 +277,32 @@ export default function PlaygroundPage() {
               className="hidden dark:block"
             />
           </div>
-          <div className="hidden h-full flex-col md:flex">
-            <div className="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
-              <h2 className="text-lg font-semibold w-full">Still in beta 🚀</h2>
+          <div className="hidden h-full flex-col md:flex ">
+            <div className="container relative mb-1.5 mt-8 flex items-start justify-between space-y-2 pb-4 pt-0 sm:flex-row sm:items-end sm:space-y-0.5 md:h-16">
+              <div className="flex w-40  flex-col gap-2">
+                <Label className="translate-x-2.5">
+                  What is the component name?
+                </Label>
+                <Input
+                  type="text"
+                  className="note-border w-[auto]"
+                  value={componentName}
+                  onChange={(e) => setComponentName(e.target.value)}
+                  placeholder="Enter Component name)"
+                />
+              </div>
+
               <PropsEditor
                 propsArray={propsArray}
                 handlePropChange={handlePropChange}
                 addNewProp={addNewProp}
+                removeProp={removeProp}
               />
-              <Input
-                type="text"
-                value={componentName}
-                onChange={(e) => setComponentName(e.target.value)}
-                placeholder="Enter Component name)"
-              />
-              <div className="ml-auto flex w-full space-x-2 sm:justify-end">
-                <PresetSelector presets={presets} />
-                <PresetSave />
-                <div className="hidden space-x-2 md:flex">
-                  <CodeViewer />
-                  <PresetShare />
-                </div>
-                <PresetActions />
-              </div>
+              <div className="ml-auto flex w-full space-x-2 sm:justify-end"></div>
             </div>
             <Tabs defaultValue="insert" className="flex-1">
               <div className=" h-full py-6">
-                <div className="grid h-full items-stretch gap-6 md:grid-cols-[1fr_200px] w-full pr-8">
+                <div className="grid h-full w-full items-stretch gap-6 pr-8 md:grid-cols-[1fr_200px]">
                   <div className="hidden flex-col space-y-4 sm:flex md:order-2">
                     <ModelSelector types={types} models={models} />
                     <MaxLengthSelector defaultValue={[256]} />
@@ -300,20 +324,32 @@ export default function PlaygroundPage() {
                           </svg>
                           aa
                         </button>
-                        <Editor
-                          height="60vh"
-                          defaultLanguage="javascript"
-                          defaultValue={code}
-                          className="rounded-full"
-                          options={{
-                            minimap: {
-                              enabled: false,
-                            },
-                            wordWrap: "on",
-                            renderValidationDecorations: "off",
+                        <motion.div
+                          initial={{ opacity: 0, scale: 0.9 }}
+                          animate={{ opacity: 1, scale: 1 }}
+                          exit={{ opacity: 0, y: 10 }}
+                          transition={{
+                            damping: 15,
+                            delay: 1,
                           }}
-                          onChange={handleEditorChange}
-                        />
+                        >
+                          <Editor
+                            height="60vh"
+                            defaultLanguage="javascript"
+                            defaultValue={code}
+                            className="rounded-full"
+                            options={{
+                              minimap: {
+                                enabled: true,
+                              },
+                              wordWrap: "on",
+                              FormData: true,
+
+                              renderValidationDecorations: "off",
+                            }}
+                            onChange={handleEditorChange}
+                          />
+                        </motion.div>
                         <div className="flex items-center space-x-2">
                           <Button>Submit</Button>
                           <Button variant="secondary">
@@ -325,22 +361,45 @@ export default function PlaygroundPage() {
                     </TabsContent>
                     <TabsContent value="insert" className="mt-0 border-0 p-0">
                       <div className="flex flex-col space-y-4">
-                        <div className="grid h-full grid-rows-2 gap-6 lg:grid-cols-2 lg:grid-rows-1">
+                        <motion.div
+                          initial={{ opacity: 0, x: 20 }}
+                          animate={{ opacity: 1, x: 0 }}
+                          exit={{ opacity: 0, y: 10 }}
+                          transition={{
+                            damping: 15,
+                            delay: 1,
+                          }}
+                          className="grid h-full grid-rows-2 gap-6 lg:grid-cols-2 lg:grid-rows-1"
+                        >
                           <Editor
                             height="60vh"
                             defaultLanguage="javascript"
                             defaultValue={code}
                             className="rounded-full"
                             options={{
-                              minimap: { enabled: false },
                               formatOnPaste: true,
                               formatOnType: true,
                               wordWrap: "on",
                               renderValidationDecorations: "off",
+                              minimap: { enabled: false },
+                              lineNumbers: "off",
+                              lineDecorationsWidth: 12,
+                              suggest: {
+                                showFiles: false,
+                              },
                             }}
                             onChange={handleEditorChange}
                           />
-                          <div className="relative">
+                          <motion.div
+                            initial={{ opacity: 0, x: 20 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            exit={{ opacity: 0, y: 10 }}
+                            transition={{
+                              damping: 15,
+                              delay: 1,
+                            }}
+                            className="relative"
+                          >
                             <button
                               onClick={handleCopy}
                               className="absolute right-0 top-0 z-50 rounded-md border border-zinc-700 bg-zinc-900 p-2"
@@ -359,16 +418,22 @@ export default function PlaygroundPage() {
                               defaultLanguage="javascript"
                               value={jsx}
                               options={{
-                                minimap: {
-                                  enabled: false,
-                                },
+                                formatOnPaste: true,
+                                formatOnType: true,
                                 wordWrap: "on",
                                 renderValidationDecorations: "off",
+                                minimap: { enabled: false },
+                                lineNumbers: "off",
+                                lineNumbersMinChars: 3,
+                                lineDecorationsWidth: 12,
+                                suggest: {
+                                  showFiles: false,
+                                },
                               }}
                               theme="vs-dark"
                             />
-                          </div>{" "}
-                        </div>
+                          </motion.div>
+                        </motion.div>
                         <div className="flex items-center space-x-2">
                           <Button>Submit</Button>
                           <Button variant="secondary">
@@ -412,10 +477,21 @@ export default function PlaygroundPage() {
                     </TabsContent>
                   </div>
                 </div>
+                <h2 className="absolute right-0  top-0 w-full text-lg font-semibold">
+                  Still in beta 🚀
+                </h2>
               </div>
             </Tabs>
+            <div className="m-3 flex gap-2 p-3">
+              <PresetSelector presets={presets} />
+
+              <PresetSave />
+              <CodeViewer />
+              <PresetShare />
+              <PresetActions />
+            </div>
           </div>
-        </div>
+        </motion.div>
       </AppContext.Provider>
     </>
   )
