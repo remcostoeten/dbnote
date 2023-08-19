2cf14e07c124cb4d57fd7bc486919bf42eedb09e
Remco Stoeten <remcostoeten@hotmail.com>
2023-08-19 15:58:34 +0200

    typo

--- config/dashboard.ts
+++ config/dashboard.ts
@@ -17,64 +17,7 @@ export const dashboardConfig: DashboardConfig = {
       title: "Account",
       href: "/dashboard/account",
       icon: "UserIcon",
-    },import { DashboardConfig } from "types"
-
-    export const dashboardConfig: DashboardConfig = {
-      mainNav: [
-        {
-          title: "Documentation",
-          href: "/docs",
-        },
-        {
-          title: "Support",
-          href: "/support",
-          disabled: true,
-        },
-      ],
-      sidebarNav: [
-        {
-          title: "Account",
-          href: "/dashboard/account",
-          icon: "UserIcon",
-        },
-        {
-          title: "Vapes",
-          href: "/dashboard/vapes",
-          icon: "VapeIcon",
-        },
-        {
-          title: "Posts",
-          href: "/dashboard",
-          icon: "DataIcon",
-        },
-        {
-          title: "Data",
-          href: "/dashboard/data",
-          icon: "ListIcon",
-        },
-        {
-          title: "Work log",
-          href: "/dashboard/users",
-          icon: "NotesIcon",
-        },
-        {
-          title: "Expenses",
-          href: "/dashboard/expenses",
-          icon: "ExpensesIcon",
-        },
-        {
-          title: "Uitzet",
-          href: "/producitvity-tools/uitzet",
-          icon: "DataIcon",
-        },
-        {
-          title: "Settings",
-          href: "/producitvity-tools/settings",
-          icon: "SettingsIcon",
-        },
-      ],
-    }                     
-    
+    },
     {
       title: "Vapes",
       href: "/dashboard/vapes",
